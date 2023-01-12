34de1a3816774970e9ebe6a86419a57ecb2a4607
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2023-01-12 14:16:39 -0600

    trying to make a modal

--- JS/modal.js
+++ JS/modal.js
@@ -1,26 +1,17 @@
-//Get the modal
-var modal = document.getElementById("testModal");
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
 
-//Get the button that opens the modal
-var button = document.getElementById("openButton");
-
-//Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-//Open the modal when button is clicked
-button.onclick = function() {
-    modal.style.display = "block";
+function toggleModal() {
+    modal.classList.toggle("show-modal");
 }
 
-//When the user clicks on <span>, close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-//When the user clicks outside fo the modal, it closes
-window.onclick = function(event) {
-    if (event.target == modal)
-    {
-        modal.style.display = "none";
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
     }
 }
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
